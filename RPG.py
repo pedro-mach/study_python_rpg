197b2945c63cf32ad71f836350b1d9e524539e81
Pedro H. Machado <pedroh.machadodeoliveira@gmail.com>
2024-08-11 09:49:06 -0300

    Update RPG.py
    Player + exp

--- RPG.py
+++ RPG.py
@@ -13,6 +13,21 @@ from random import randint
 
 npc_list = []
 
+start_level = 1
+dano_player = (start_level + (randint(10, 15)))*2,
+Player = {
+    "nome": "Pedro",
+    "level": 1,
+    "exp": 0,
+    "exp_max": 30,
+    "hp": 80,
+    "hp_max": 200,
+    "dano": dano_player
+}
+print (Player
+       )
+
+
 def create_npc():
     
     level = randint(1, 30) # level
@@ -43,6 +58,7 @@ def create_npc():
         "Level": level,
         "damage": dano, 
         "hp": 50 *(level/2), # var de hp - mudar
+        "exp": (dano - level)*2,
         "Raridade": raridade,
         "Class": Classe
         
@@ -58,7 +74,7 @@ def rep_npc(n_npcs): # adiciona na lista
 def show_npcs(): # fromatação
     for npc in npc_list:
         print(
-            f"Nome: {npc['Nome']} | Level: {npc['Level']} | Damage: {npc['damage']} | HP: {npc['hp']} | Classe: {npc['Class']} | Raridade: {npc['Raridade']}"
+            f"Nome: {npc['Nome']} | Level: {npc['Level']} | HP: {npc['hp']} | Damage: {npc['damage']} | Classe: {npc['Class']} | Raridade: {npc['Raridade']} | XP: {npc['exp']}"
         )
     
 rep_npc(5) # quantia
