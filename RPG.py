77c0c9129d63981cba702dc07a3ed5035cda41ec
Pedro H. Machado <pedroh.machadodeoliveira@gmail.com>
2024-09-15 20:51:26 -0300

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -5,8 +5,39 @@ import sys
 import time
 
 def reset():
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
+    continuar = str(input("Digite 'S' para continuar ou 'N' para sair: "))
+    if continuar.upper() == "S":
+        print("Vamos nessa então !!!")
+        space()
+        time.sleep(1)
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+    else:
+        print("Saindo do Jogo...")            
+        space()
+        time.sleep(0.3)
+        print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+        time.sleep(0.3)
+        print("░░█░░░░█▀▀▀▀░█▀▀█░█░░█░█░█▄░░█░█▀▀█░░")
+        time.sleep(0.3)
+        print("░░█░░░░█◼◼◼░░█▄▄█░█░░█░█░█░█░█░█░▄▄░░")
+        time.sleep(0.3)
+        print("░░█▄▄█░█▄▄▄▄░█░░█░▀▄▄▀░█░█░░▀█░█▄▄█░░")
+        time.sleep(0.3)
+        print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+        time.sleep(0.3)
+        print("░█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█░")
+        time.sleep(0.3)
+        print("░█░██░██░██░██░██░██░██░██░██░██░░░█░")
+        time.sleep(0.3)
+        print("░█░██░██░██░██░██░██░██░██░██░██░░░█░")
+        time.sleep(0.3)
+        print("░█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█░")
+        time.sleep(0.3)
+        print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+        space()
+        time.sleep(2)
+        sys.exit()
 
 def space():
     print("◼" * 100)
@@ -43,11 +74,13 @@ continuar = input("Digite 'TANK' para +10 de Armadura, 'ASSASSINO' para +10 de d
 level = 1
 dano_player = (level + (randint(10, 15)))*2
 hp = 50 *(randint(2, 5)/2)
+if continuar.upper() == "TANK":
+    hp = hp + 50
 if continuar.upper() == "ASSASSINO":
-        dano_player += 10
+    dano_player += 10
 Player = {     
     "nome": nome,
-    "level": 1,
+    "level": level,
     "exp": 0,
     "exp_max": 20,
     "hp": hp,
@@ -95,14 +128,20 @@ def create_npc():
     exp = (dano_original)*2
     if exp <=0:
         exp = 0
-    
+    tag = "NPC_"
     nome = len(npc_list) + 1
+    name = tag + str(nome)
     new_npc = { # criação de NPCs e atribuições
-        "Nome": nome,
+        "ID": nome,
+        "Nome": name,
+        "Level": level,
+        "Dano": dano_original,
+        "damage": dano,
+        "Nome": name,
         "Level": level,
         "Dano": dano_original,
         "damage": dano, 
-        "hp": 50 *(level/2), # var de hp - mudar
+        "hp": 40 *(level/2), # var de hp - mudar
         "exp": exp,
         "Raridade": raridade,
         "Class": Classe
@@ -117,29 +156,38 @@ def rep_npc(n_npcs): # adiciona na lista
 
 rep_npc(5)
 
-print("Selecione um NPC pelo ID para iniciar o combate")
-print("↓↓↓")
+def cond_vitoria():
+     todos_mortos = all(npc['Nome'] == 'NPC Morto' for npc in npc_list)
+     return todos_mortos
 
 def show_npcs(): # fromatação
     for npc in npc_list:
         if npc['hp'] > 0:
             print(
-            f"ID {npc['Nome']} | Level: {npc['Level']} | HP: {npc['hp']} | Dano-origal: {npc['Dano']} | Dano: {npc['damage']} | Classe: {npc['Class']} | Raridade: {npc['Raridade']} | XP: {npc['exp']}"
+            f"ID {npc['ID']} | {npc['Nome']} | Level: {npc['Level']} | HP: {npc['hp']} | Dano-origal: {npc['Dano']} | Dano: {npc['damage']} | Classe: {npc['Class']} | Raridade: {npc['Raridade']} | XP: {npc['exp']}"
             )
         else:
+            npc['Nome'] = "NPC Morto"
             print(
-               f"ID {npc['Nome']} | NPC Morto"
+               f"{npc['Nome']}"
             )
+            if cond_vitoria():
+                space()
+                print("Você venceu!")
+                time.sleep(1)
+                reset()
 
-def select():
-    show_npcs()
-    npc = int(input("Escolha um NPC: "))
-    if  0 <= npc <= 5:
-                return npc_list[npc - 1]
-    else:
-        print("Escolha um NPC válido!")
-        space()
-        select()
+def select(): 
+        print("Selecione um NPC pelo ID para iniciar o combate")
+        print("↓↓↓")
+        show_npcs()
+        npc = int(input("Escolha um NPC: "))
+        if  0 <= npc <= 5:
+            return npc_list[npc - 1]
+        else:
+            print("Escolha um NPC válido!")
+            space()
+            select()
         
     
                 
@@ -150,23 +198,27 @@ def final():
         Player['exp_max'] =30 
         Player['hp_max'] += 50
         Player['hp'] = Player['hp_max']
-        print(f"→Level Up! Novo Level: {Player['level']}")   
+        print(f"→Level Up! Novo Level: {Player['level']}") 
         
 def batalha():
     npc = select()
     space()
     while Player['hp'] > 0 and npc['hp'] > 0:
-            Player['hp'] -= npc['damage']
             npc['hp'] -= Player['dano']
-            print(
-                f"Nome: {Player['nome']} | HP: {Player['hp']} | Damage: {Player['dano']}"
-            )
+            Player['hp'] -= npc['damage']
+            if npc['hp'] < 0:
+                npc['hp'] = 0
+            if Player['hp'] < 0:
+                Player['hp'] = 0
             print(
                 f"Nome: {npc['Nome']} | HP: {npc['hp']} | Damage: {npc['damage']}"
             )
+            print(
+                f"Nome: {Player['nome']} | HP: {Player['hp']} | Damage: {Player['dano']}"
+            )
             time.sleep(0.5)
     space()
-    if npc['hp'] < 0:
+    if npc['hp'] <= 0:
         Player['exp'] += npc['exp']
         print(f"→ NPC derrotado || Exp Ganha:{npc['exp']}")
         final()
@@ -174,43 +226,11 @@ def batalha():
         space()
         print( f"Nome: {Player['nome']} | Level: {Player['level']} | HP: {Player['hp']} | Damage: {Player['dano']} | Exp: {Player['exp']} / {Player['exp_max']}")
         space()
-        print("Selecione um NPC pelo ID para iniciar o combate")
-        print("↓↓↓")
         batalha()
         
-    if Player['hp'] < 0:
+    if Player['hp'] <= 0:
         print(f"→ Você foi derrotado !!!")
         print("→ Vamos novamente?")
-        continuar = input("Digite 'S' para continuar ou 'N' para sair: ")
-        if continuar.upper() == "S":
-            print("Vamos nessa então !!!")
-            space()
-            time.sleep(2)
-            reset()
-        else:
-            print("Saindo do Jogo...")            
-            space()
-            time.sleep(0.3)
-            print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
-            time.sleep(0.3)
-            print("░░█░░░░█▀▀▀▀░█▀▀█░█░░█░█░█▄░░█░█▀▀█░░")
-            time.sleep(0.3)
-            print("░░█░░░░█◼◼◼░░█▄▄█░█░░█░█░█░█░█░█░▄▄░░")
-            time.sleep(0.3)
-            print("░░█▄▄█░█▄▄▄▄░█░░█░▀▄▄▀░█░█░░▀█░█▄▄█░░")
-            time.sleep(0.3)
-            print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
-            time.sleep(0.3)
-            print("░█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█░")
-            time.sleep(0.3)
-            print("░█░██░██░██░██░██░██░██░██░██░██░░░█░")
-            time.sleep(0.3)
-            print("░█░██░██░██░██░██░██░██░██░██░██░░░█░")
-            time.sleep(0.3)
-            print("░█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█░")
-            time.sleep(0.3)
-            print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
-            space()
-            time.sleep(2)
-            sys.exit()
+        reset()
+        
 batalha()
